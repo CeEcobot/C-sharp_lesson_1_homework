9db9653433c970b05f1fb2d251aafa25b8904916
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-14 22:20:19 +0300

    Find max number

---
+++ Homework/Homework1_Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (max > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine($"{max}");
+}
+else
+{
+  Console.Write("max = ");
+  Console.WriteLine($"{num2}");  
+}
