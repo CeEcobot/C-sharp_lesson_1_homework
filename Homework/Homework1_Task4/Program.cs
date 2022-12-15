50a1827bde3b85173e0af0100d2cbea571816b20
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-15 14:22:07 +0300

    Even numbers in period

---
+++ Homework/Homework1_Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count < num)
+{
+    if(num % 2 == 0)
+    Console.WriteLine($"{num}");
+    num--;
+}
