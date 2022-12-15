de93fde4a1ad20f600a5465d715058ffd4ec5767
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-15 08:07:02 +0300

    Even number

--- Homework/Homework1_Task3/Program.cs
+++ Homework/Homework1_Task3/Program.cs
@@ -1,8 +1,8 @@
-﻿//double numberA = 12;
-//double numberB = 5;
-//Console.WriteLine(numberA / numberB);
-Console.Write("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num1 / num2);
+﻿Console.Write("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+
+
+if(num % 2 == 0)
+Console.WriteLine($"{num} - четное число");
+else
+Console.WriteLine($"{num} - нечетное число");
