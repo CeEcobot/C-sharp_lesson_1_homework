5f4e395434ad131e5335f9c3cd7c71fe9a72d76b
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-15 07:01:56 +0300

    Delete string: if num1 > max

--- Homework/Homework1_Task2/Program.cs
+++ Homework/Homework1_Task2/Program.cs
@@ -6,7 +6,6 @@ Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 
-if (num1 > max) max = num2;
 if (num2 > max) max = num2;
 if (num3 > max) max = num3;
 
