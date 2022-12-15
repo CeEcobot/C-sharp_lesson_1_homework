4f7dc6db10d350766707432dface805f6f27a6ee
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-15 07:05:55 +0300

    1

---
+++ Homework/Homework1_Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿//double numberA = 12;
+//double numberB = 5;
+//Console.WriteLine(numberA / numberB);
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num1 / num2);
