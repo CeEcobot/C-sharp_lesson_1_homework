401abefb0955bdaf3fae1e4eed2aff9ed5b0b2fc
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-15 17:17:37 +0300

    Number digit

---
+++ Homework/Homework1_Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num != 0)
+{
+    Console.WriteLine(num % 10);
+    num /= 10;                
+    }
+
+// Решил как разбить на разряды, но не могу понять как вывести нужный третий разряд слева
